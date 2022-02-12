ab8cce7998792696a626ece964aaae2928e9eece
Sondouni <ezicland@gmail.com>
2022-02-13 00:51:43 +0900

    2022-02-12

---
+++ src/main/java/com/gsvl/oneul/common/Const.java
@@ -0,0 +1,7 @@
+package com.gsvl.oneul.common;
+
+public class Const {
+    public final static String F_COOKERY = "f_cookery";
+    public final static String F_WORLDDIV = "f_worlddiv";
+    public final static String IGD = "igd";
+}
--- src/main/java/com/gsvl/oneul/food/FoodController.java
+++ src/main/java/com/gsvl/oneul/food/FoodController.java
@@ -1,11 +1,18 @@
 package com.gsvl.oneul.food;
 
+import com.gsvl.oneul.common.Const;
+import com.gsvl.oneul.food.model.FoodConNmEntity;
+import com.gsvl.oneul.food.model.FoodConditionBean;
 import com.gsvl.oneul.food.model.FoodConditionEntity;
 import com.gsvl.oneul.food.model.FoodResultVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -13,9 +20,14 @@ import java.util.List;
 public class FoodController {
     @Autowired
     private FoodService foodService;
+    @Autowired
+    private ApplicationContext appCon;
 
     @GetMapping()
-    public String goFood(){
+    public String goFood(Model model){
+        model.addAttribute(Const.F_COOKERY,(List<FoodConNmEntity>) appCon.getBean("f_cookery"));
+        model.addAttribute(Const.F_WORLDDIV,(List<FoodConNmEntity>) appCon.getBean("f_worlddiv"));
+        model.addAttribute(Const.IGD,(List<FoodConNmEntity>) appCon.getBean("igd"));
         return "/food/condition";
     }
 
