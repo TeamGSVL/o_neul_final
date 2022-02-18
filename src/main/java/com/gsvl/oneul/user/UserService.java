b36a5f153422f89781cd960079c7f5054e738843
Sondouni <ezicland@gmail.com>
2022-02-18 16:13:54 +0900

    2022-02-17 food zzim 등록 완료

--- src/main/java/com/gsvl/oneul/user/UserService.java
+++ src/main/java/com/gsvl/oneul/user/UserService.java
@@ -1,6 +1,8 @@
 package com.gsvl.oneul.user;
 
+import com.gsvl.oneul.common.security.AuthenticationFacade;
 import com.gsvl.oneul.common.security.SecurityUserService;
+import com.gsvl.oneul.user.model.UserDTO;
 import com.gsvl.oneul.user.model.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -11,6 +13,7 @@ public class UserService {
     @Autowired private UserMapper mapper;
     @Autowired private PasswordEncoder passwordEncoder;
     @Autowired private SecurityUserService securityUserService;
+    @Autowired private AuthenticationFacade auth;
 
     public int join(UserEntity entity){
         //패스워드 암호화
@@ -19,6 +22,26 @@ public class UserService {
         int result = securityUserService.join(entity);
         return result;
     }
+
+    //찜목록
+    //음식찜
+    public int isZzimFood(UserDTO dto){
+        return mapper.isZzimFood(dto);
+    }
+    public int insZzimFood(UserDTO dto){
+        return mapper.insZzimFood(dto);
+    }
+    public int delZzimFood(UserDTO dto){
+        return mapper.delZzimFood(dto);
+    }
+
+
+
+
+    public int isZzimJmt(UserDTO dto){
+        return mapper.isZzimJmt(dto);
+    }
+
 
 
 }
